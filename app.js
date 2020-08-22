28f8025d66884938aacae1fa930eaf8737a89af8
김수혁 <soohyeok8901@gmail.com>
2020-08-22 16:42:03 +0900

    분기 생성용 커밋입니다.

---
+++ app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+
+var router = require('./routes')(app);
+var connect = require('./models');
+
+var app = express();
+connect();  //mongoDB 와 연결
+
+
+var port = 3000;
+app.set('port', port);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+//내장된 body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//body-parser
+app.use(bodyParser.raw());
+app.use(bodyParser.text());
+//cookie-parser
+app.use(cookieParser());
+//path
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//라우팅
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  // console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.exports = app;
---
+++ models/index.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+module.exports =() =>{
+    //connect 함수
+    const connect = () =>{
+       if(process.env.NODE_ENV !== 'production'){
+           mongoose.set('debug', true);
+       }
+
+       mongoose.connect('mongodb://soohyeok:asd123@localhost:27017/admin',{
+           dbName: 'FotBB-DB',
+        },(error) =>{ //마지막 인자(error) -> 연결 여부 확인 
+            if (error) {
+                console.log('FotB connect error',error);
+            }else{
+                console.log('FotB connected');    
+            }
+        });
+    };
+
+    //mongoDB와 노드 연결
+    connect();
+    //몽구스 커넥션에 이벤트 리스너를 담, 에러 발생시 에러 내용 기록 및 연결 종료 시 재연결시도
+    mongoose.connection.on('error',(error)=>{
+        console.error('connection error',error);
+    });
+    mongoose.connection.on('disconnected',()=>{
+        console.error('disconnected. try connect to FotB');
+    });
+
+
+
+    //스키마 연결부
+    /*
+   
+    require('./rank');
+    require('./every-stage-score);
+    */
+   require('./user');
+
+};
