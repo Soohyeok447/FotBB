671c6f8207660c34343ad1ada0e1fc60b9f14014
ksh <soohyeok8901@gmail.com>
2020-11-22 07:56:55 +0000

    local환경에서 webpack-dev-server을 이용해 개발하기 위한 저장용 커밋

--- public/js/react/wordrelay.jsx
+++ public/js/react/wordrelay.jsx
@@ -1,14 +1,45 @@
 const React = require('react');
-const {Component}= React;
+const { useState, useRef }= React;
 
-class WordRelay extends Component {
-    state = {
-        text: 'Hello, webpack 저는 설정이 복잡합니다ㅎㅎ',
+const WordRelay = () => {
+    /* state */
+    const [word,setWord] = useState('기러기');
+    const [value,setValue] = useState('');
+    const [result,setResult] = useState('');
+    const inputRef = useRef(null);
+
+    /* method */
+    const onSubmitForm = (e) =>{
+        e.preventDefault();
+        if(word[word.length -1 ]=== value[0]){
+            setResult('딩동댕');
+            setWord(value);
+            value('');            
+            inputRef.current.focus();
+        }else{
+            setResult('땡');
+            setValue('');
+            inputRef.current.focus();
+        }
     };
 
-    render(){
-    return <h1>{this.state.text}</h1>
-    }
+    
+    const onChangeInput = (e)=>{
+        setValue(e.target.value);
+    };
+
+
+    /* render */
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit= {onSubmitForm}>
+                <input ref={inputRef} value={value} onChange={onChangeInput} />
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    )
 }
 
 module.exports = WordRelay;
